12408e7d2a997eceff0d7c48d87439c3d8cd37d3
voidash <ashish.thapa477@gmail.com>
2023-03-21 14:57:31 +0545

    [refactor] different folder for routes, reads configuration values from configuration.toml on root, add config to cargo.toml "

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 use std::net::TcpListener;
+use z2p::startup::run;
+use z2p::configuration::{get_configuration, self};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    let configuration = get_configuration().expect("failed to read configuration file");
+
     let listener = TcpListener::bind("127.0.0.1:8000").expect("failed to bind address");
-
     println!("{:?}", listener);
-
-    z2p::run(listener)?.await
+    run(listener)?.await
 }
